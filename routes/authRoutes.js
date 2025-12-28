e465056311293175c992c193c5da397b67787b41
Chirag Nagra <chiragnagra256@gmail.com>
2025-12-28 10:51:06 +0530

    feat: complete user registration with browser form and MongoDB

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,6 @@ const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
-// TEMP test route (browser-friendly)
-router.get("/register", (req, res) => {
-  res.send("Register route is working. Use POST to register users.");
-});
 
 router.post("/register", register);
 router.post("/login", login);
