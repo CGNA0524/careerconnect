2e51bb04c4797a441415dd9805cc7f2cb39234eb
Chirag Nagra <chiragnagra256@gmail.com>
2025-12-28 11:54:16 +0530

    feat: verify login flow and JWT protected routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,10 @@ const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
+router.get("/login", (req, res) => {
+  res.send("Login route is working. Use POST to login.");
+});
+
 
 router.post("/register", register);
 router.post("/login", login);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -13,7 +14,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("CareerConnect backend is running 🚀");
